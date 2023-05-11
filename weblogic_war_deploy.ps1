4a00816582e6ec960b8ffa1670b8ff9811c0fd28
mahfuz-1994 <117646198+mahfuz-1994@users.noreply.github.com>
2023-05-11 17:10:09 +0530

    Update weblogic_war_deploy.ps1

--- weblogic_war_deploy.ps1
+++ weblogic_war_deploy.ps1
@@ -8,6 +8,8 @@ $target = "AdminServer"
 
 # Deploy the application using weblogic.Deployer
 #"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
+#IF YOU WANT TO UNDEPLOY USE BELOW SYNTAX
+#java -cp weblogic.jar weblogic.Deployer -adminurl http://localhost:7001 -username weblogic -password weblogic@123 -undeploy -name devopstool
 java -cp "D:\Oracle\Middleware\Oracle_Home\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
  -verbose `
  -noexit `
